3301caaf3b752b9e91af32377b918adeb7e43359
Rinne <i@yiheng.moe>
2022-02-27 14:17:51 +0800

    [gateway] gateway service

---
+++ gateway/src/main/java/moe/yiheng/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package moe.yiheng.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author rinne
+ * @Date 2022/2/27
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
